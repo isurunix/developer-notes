e308ee819ac1a6c99006a0c7d429cf9c0235c8e9
Isuru Pathirana <isurunix@gmail.com>
2023-10-26 23:44:33 +0530

    Refactor workflow

--- convert_to_hugo.sh
+++ convert_to_hugo.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 pip install obsidian-to-hugo
-python -m obsidian_to_hugo --obsidian-vault-dir=system-design --hugo-content-dir=isurubuddhika.dev/content/
+python -m obsidian_to_hugo --obsidian-vault-dir=system-design --hugo-content-dir=.converted/
