2ead9556b7498cade39af5718fb51be1a8d7f102
Isuru Pathirana <isurunix@gmail.com>
2023-10-26 23:06:45 +0530

    Update shell script

--- push_to_hugo.sh
+++ push_to_hugo.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
-python -m obsidian_to_hugo --obsidian-vault-dir=system-design --hugo-content-dir=isurubuddhika.dev/content/notes
 pip install obsidian-to-hugo
-git config user.email ${{ secrets.EMAIL }}
+python -m obsidian_to_hugo --obsidian-vault-dir=system-design --hugo-content-dir=isurubuddhika.dev/content/
+git config user.email $EMAIL
 git config user.name "Isuru Buddhika Pathirana"
 cd isurubuddhika.dev
+git checkout main
 git commit -a -m "Publishing from Obsidian"
 git push --recurse-submodules=on-demand
